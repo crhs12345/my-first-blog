c7240200bea9c9967097534f371aaf5b64168947
seong-gyu <crhs12345@gmail.com>
2019-11-03 09:45:18 +0900

    Post_list view added

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Post
-from .models import WeirdPost
+from .models import Post, WeirdPost
 
 admin.site.register(Post)
 admin.site.register(WeirdPost)
